6a3f82274706315e6f82de610b58b69468954021
Archana0705 <archanathoshik@gmail.com>
2026-04-27 13:21:22 +0530

    global-redesign-for-login-stagging-url-changed

--- redesign/assets/js/global.js
+++ redesign/assets/js/global.js
@@ -1,7 +1,8 @@
-const API_BASE_URL = 'http://localhost/ssp/ssp_3/department/ssp_backend_1/public/api';
+//const API_BASE_URL = 'http://localhost/ssp/ssp_3/department/ssp_backend_1/public/api';
+const API_BASE_URL = 'http://192.168.4.251/state_scholarship_portal_api/ssp_3/public/api';
 
 let config = {
-    ssp_api_url : 'http://192.168.4.251/state_scholarship_portal_api/home_page/api/',
+    ssp_api_url: 'http://192.168.4.251/state_scholarship_portal_api/home_page/api/',
     home_api_url: 'http://192.168.4.251/state_scholarship_portal_api/home_page/api',
     dept_api_url: 'http://192.168.4.251/state_scholarship_portal_api/home_page/api/department/',
     sso_api_url: 'http://192.168.4.251/state_scholarship_portal_api/sso/api'
